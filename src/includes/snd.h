fd8c75115f0c5fd4d95632940f50d6d289fc24a2
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2015-05-23 14:38:20 +0000

    Further improving sound emulation.

--- src/includes/snd.h
+++ src/includes/snd.h
@@ -1,11 +1,12 @@
 void SND_IO_Handler(void);
 void Sound_Reset(void);
 
-void snd_send_sound_out(void);
+void snd_send_normal_samples(void);
+void snd_send_double_samples(bool repeat);
 void snd_queue_poll(Uint8 *buf, int len);
-void snd_start_output(void);
+void snd_start_output(Uint8 mode);
 void snd_stop_output(void);
-void snd_change_output_freq(int frequency);
+void snd_gpo_access(Uint8 data);
 
 
 struct {
@@ -13,7 +14,3 @@ struct {
     Uint32 size;
     Uint32 limit;
 } snd_buffer;
-
-/* Valid frequencies */
-#define SND_FREQ_DOUBLE 22050
-#define SND_FREQ_NORMAL 44100
