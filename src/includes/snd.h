8021ac0cab9de775fc49fe5de1316c3da10e239e
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2015-05-24 09:09:13 +0000

    Adding primitive lowpass filter.

--- src/includes/snd.h
+++ src/includes/snd.h
@@ -1,7 +1,7 @@
 void SND_IO_Handler(void);
 void Sound_Reset(void);
 
-void snd_queue_poll(Uint8 *buf, int len);
+void sndout_queue_poll(Uint8 *buf, int len);
 void snd_start_output(Uint8 mode);
 void snd_stop_output(void);
 void snd_gpo_access(Uint8 data);
