2ed5c6bb6312d7fe3b7927f2005026e692333bb5
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2015-09-27 21:53:12 +0000

    Some more printer GUI work.

--- branches/branch_mmu/src/gui-sdl/dlgPrinter.c
+++ branches/branch_mmu/src/gui-sdl/dlgPrinter.c
@@ -20,27 +20,35 @@ const char DlgPrinter_fileid[] = "Previous dlgPrinter.c : " __DATE__ " " __TIME_
 #define DLGPRINT_B5         8
 #define DLGPRINT_LEGAL      9
 
-#define DLGPRINT_EXIT       10
+#define DLGPRINT_BROWSE     12
+#define DLGPRINT_DIRECTORY  13
 
+#define DLGPRINT_EXIT       14
 
+char dlgprint_dirname[64];
 
 /* The Printer options dialog: */
 static SGOBJ printerdlg[] =
 {
-	{ SGBOX, 0, 0, 0,0, 39,17, NULL },
-    { SGTEXT, 0, 0, 12,1, 9,1, "Printer options" },
+	{ SGBOX, 0, 0, 0,0, 47,23, NULL },
+    { SGTEXT, 0, 0, 16,1, 9,1, "Printer options" },
 
     { SGBOX, 0, 0, 1,4, 22,8, NULL },
     { SGCHECKBOX, 0, 0, 2,5, 19,1, "Printer connected" },
     
-    { SGBOX, 0, 0, 24,4, 14,8, NULL },
+    { SGBOX, 0, 0, 24,4, 22,8, NULL },
 	{ SGTEXT, 0, 0, 25,5, 30,1, "Paper size:" },
 	{ SGRADIOBUT, 0, 0, 25,7, 4,1, "A4" },
     { SGRADIOBUT, 0, 0, 25,8, 8,1, "Letter" },
     { SGRADIOBUT, 0, 0, 25,9, 4,1, "B5" },
     { SGRADIOBUT, 0, 0, 25,10, 7,1, "Legal" },
     
-	{ SGBUTTON, SG_DEFAULT, 0, 9,14, 21,1, "Back to main menu" },
+    { SGBOX, 0, 0, 1,13, 45,5, NULL },
+    { SGTEXT, 0, 0, 2,14, 19,1, "Directory to save printer output:" },
+    { SGBUTTON, 0, 0, 37,14, 8,1, "Browse" },
+    { SGTEXT, 0, 0, 2,16, 43,1, dlgprint_dirname },
+    
+	{ SGBUTTON, SG_DEFAULT, 0, 13,20, 21,1, "Back to main menu" },
 	{ -1, 0, 0, 0,0, 0,0, NULL }
 };
 
@@ -86,12 +94,22 @@ void DlgPrinter_Main(void)
             break;
     }
     
+    File_MakeValidPathName(ConfigureParams.Printer.szPrintToFileName);
+    File_ShrinkName(dlgprint_dirname, ConfigureParams.Printer.szPrintToFileName,
+                    printerdlg[DLGPRINT_DIRECTORY].w);
+    
 
     /* Draw and process the dialog */
     
 	do
 	{
 		but = SDLGui_DoDialog(printerdlg, NULL);
+        
+        if (but == DLGPRINT_BROWSE) {
+            SDLGui_DirectorySelect(dlgprint_dirname,
+                                   ConfigureParams.Printer.szPrintToFileName,
+                                   printerdlg[DLGPRINT_DIRECTORY].w);
+        }
 	}
 	while (but != DLGPRINT_EXIT && but != SDLGUI_QUIT
 	       && but != SDLGUI_ERROR && !bQuitProgram);
