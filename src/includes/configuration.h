e975d1ea5825b49431616428e99613af67ae0230
andreas-g1 <andreas-g1@9ba66e4b-2233-44b8-a46a-7e93b2105c9e>
2017-04-03 17:47:49 +0000

    Add support for second MO drive to GUI.

--- src/includes/configuration.h
+++ src/includes/configuration.h
@@ -346,6 +346,7 @@ void Configuration_Apply(bool bReset);
 int Configuration_CheckMemory(int *banksize);
 int  Configuration_CheckDimensionMemory(int *banksize);
 void Configuration_CheckDimensionSettings(void);
+void Configuration_CheckOpticalSettings(void);
 void Configuration_Load(const char *psFileName);
 void Configuration_Save(void);
 void Configuration_MemorySnapShot_Capture(bool bSave);
